b6eecba621d23ebc77ae002b07a87b241602e8bf
TagEater <70480030+TagEater@users.noreply.github.com>
2021-05-10 20:31:47 +0200

    Script and rensponsiveness
    I add images, did almost all scripts (expect ajax mail hanlder) and all styling for every device (i will try to handle this form soon)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,25 @@
+/* ===== NAVBAR SCRIPT ===== */
+
+window.onscroll = () => navbarHandler();
+// get require elements from document
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar-menu");
+
+// handle click event for responsive menu (toggle class active)
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("activated");
+});
+
+// check navbar position, when it's offset then add sticky class
+const navbarHandler = () => {
+	const navbar = document.getElementById("header");
+	let navbarOffset = navbar.offsetTop;
+
+	if(window.pageYOffset >= navbarOffset + 1) navbar.classList.add("sticky");
+	else { navbar.classList.remove("sticky"); }
+}
+
 /* ====== SLIDER HANDLER ====== */
 
 var slideId = 0;
@@ -16,7 +38,7 @@ const showSlides = (num) =>
 	const controls = document.getElementsByClassName("dots-menu__item");
 
 	if(num >= slides.length) slideId = 0;
-	if(num < 0) slideId = slides.length;
+	if(num < 0) slideId = slides.length - 1;
 
 	// remove class from all dots
 	for(i = 0; i < controls.length; i++)
@@ -37,18 +59,34 @@ const showSlides = (num) =>
 
 showSlides(slideId);
 
+/* ===== POPUP SCRIPT ===== */
 
-/* ===== NAVBAR SCRIPT ===== */
-
-window.onscroll = () => navbarHandler();
-
-// check navbar position, when it's offset then add sticky class
-const navbarHandler = () => {
-	const navbar = document.getElementById("header");
-	let navbarOffset = navbar.offsetTop;
-
-	if(window.pageYOffset >= navbarOffset + 1) navbar.classList.add("sticky");
-	else { navbar.classList.remove("sticky"); }
+const togglePopup = () => {
+	let popup = document.getElementById('popup');
+	popup.classList.toggle('active');
 }
 
-/* TODO: RESPONSIVE NAVABR HANDLER, POP-UP, GOOGLE MAP, MAIL HANDLER, FORM VALIDATOR */
+/* ===== GOOGLE MAPS ===== */
+
+// Initialize and add the map
+function initMap() {
+	// The location for map center
+	const location = { lat: 52.3992916, lng: 16.9047494 };
+
+	// The map center at selected location
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: location,
+		disableDefaultUI: true,
+    });
+
+    // The marker, positioned at glogowska
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+		icon: 'http://tageater.atwebpages.com/dist/images/marker.png'
+    });
+}
+
+
+initMap(); 
